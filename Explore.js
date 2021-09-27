0e0a9a17ec689f9d6e4ac4cf37ddb51e0fc32b9a
Eslam Onsy <eslamonsy56@gmail.com>
2021-09-27 22:10:28 +0200

    Explore page deployment

--- Explore.js
+++ Explore.js
@@ -2,17 +2,46 @@
 const mediaType = location.search.slice(1); //Responsible for holding the media type {movies, tv or people}
 
 var pageNumber; //Responsible for displaying which page fetched from the database
+var optionList; //Responsible for displaying the options from which the user can choose the query (upcoming, popular, etc)
 var currentDisplay; //Responsible for indicating which data should be displaying
 var displayList; //The element that display the data
 var apiData; //Responsible for holding data needed to construct the displayed list
+var queryOptions; //Responsible of holding each query option possible as index for easier access (index is sorted according to the optionList)
 
 pageInit();
 
 async function pageInit(){
     pageNumber = 1;
+    optionList = document.querySelectorAll(".mainContainer .optionList span");
     currentDisplay = "popular";
     displayList = document.querySelector(".mainContainer .content");
+    queryOptions = mediaType == "movie"? ["popular", "now_playing", "upcoming", "top_rated"]: ["popular", "airing_today", "on_the_air", "top_rated"];
+    configureOptionList();
+    configureDisplayList();
+}
 
+//Responsible for configuring all actions related to the OptionList
+function configureOptionList(){
+    for(let i = 0 ; i < 4 ; ++i){ 
+        optionList[i].id = i; 
+        optionList[i].textContent = queryOptions[i].split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+    }
+
+    optionList.forEach(option => {
+        option.addEventListener("click", ()=>{
+            optionList.forEach(element => {element.className = ""});
+            option.className = ".selected";
+
+            pageNumber = 1;
+            currentDisplay = queryOptions[option.id];
+            configureDisplayList();
+        });
+    });
+
+}
+
+//Responsible of getting the data from the api and sending it to the construction function of the display list which shows content in the page
+async function configureDisplayList(){
     await setData();
     constructList();
 }
@@ -69,12 +98,88 @@ function constructList(){
 
     displayList.appendChild(item);
     }
+    updatePageNumbers();
+}
+
+//I am responsible of configuring the page numbers at the bottom of the page
+function updatePageNumbers(){
+    // console.log(displayList);
+    const left = Math.max(parseInt(pageNumber)-2, 1);
+    const right = Math.min(parseInt(pageNumber)+3, apiData.total_pages);
+    constructPageNumbers(left, right);
+}
+
+//I am responsible of constructing the page number at the bottom of the page and giving them functionalities
+function constructPageNumbers(left, right){
+    // console.log(left, right);
+    const numberContainer = document.createElement("div");
+    numberContainer.className = "pageNumbers";
+    numberContainer.textContent = "";
+
+    //I construct the previous button
+    const previous = document.createElement("span");
+    previous.id = "previous";
+    previous.className = "number";
+        const leftArrow = document.createElement("i");
+        leftArrow.className = "fas fa-angle-left";
+        leftArrow.style = "transform: translateY(8%);"
+        previous.appendChild(leftArrow);
+    previous.append(" previous");
+
+    previous.addEventListener("click", async () => {
+        pageNumber--;
+        await configureDisplayList();
+        sendUserToTop();
+    });
+
+    if(pageNumber > 1) numberContainer.appendChild(previous);
+    
+    
+    for(let i = left ; i <= right ; ++i){
+        const spanEl = document.createElement("span");
+        spanEl.className = i == pageNumber? "number selected" : "number";
+        spanEl.textContent = i;
+        spanEl.id = i;
+
+        spanEl.addEventListener("click", async () => {
+            pageNumber = spanEl.id;
+            // console.log(pageNumber);
+            await configureDisplayList();
+            sendUserToTop();
+        })
+
+        numberContainer.appendChild(spanEl);
+    }
+
+    //I construct the next button
+    const next = document.createElement("span");
+    next.id = "next";
+    next.className = "number";
+    next.textContent = "next ";
+        const rightArrow = document.createElement("i");
+        rightArrow.className = "fas fa-angle-right";
+        rightArrow.style = "transform: translateY(8%);"
+        next.appendChild(rightArrow)
+    if(pageNumber < apiData.total_pages) numberContainer.appendChild(next);
+
+    next.addEventListener("click", async () => {
+        pageNumber++;
+        await configureDisplayList();
+        sendUserToTop();
+    });
+
+    displayList.appendChild(numberContainer);
+}
+
+//I am responsible for sending the user all the way to the top of the page
+function sendUserToTop(){
+    scroll({top:0, behavior: "smooth"});
 }
 
 //responsible for sending information to construct request and assign the api data to the variable apiData
 async function setData(){
     apiData = await constructrequest(mediaType, currentDisplay);
-    console.log(apiData);
+    // console.log(apiData);
 }
 
 //responsible for constructing the query string that will be needed for the api call then calling the api and returning the data coming from it
